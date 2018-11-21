94d1193c91e9a12c520c0c21b8cc89c8cfa9b443
LandrumTrev <landrum.trevillian@gmail.com>
2018-11-20 19:08:04 -0500

    revision

--- routes/workspaceApiRoutes.js
+++ routes/workspaceApiRoutes.js
@@ -11,14 +11,15 @@
 
 // GET all topics for project
 // GET all resources for each topic
+
 // POST new topic for project
 // POST new resource for each topic
+
 // PUT change name of a topic
 // PUT change project assignment of topic (?)
 // PUT change name of a resource
 // PUT change content of a resource
 // PUT change topic assignment of a resource (?)
-// EXAMPLE apiRoutes.js ONLY : USE new individual ____ApiRoutes.js scripts
 
 var db = require("../models");
 
