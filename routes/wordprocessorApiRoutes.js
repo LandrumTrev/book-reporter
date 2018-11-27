20cba3c2b84853aa45afe2ba3402e056047c4a98
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-11-27 00:33:54 -0500

    added wordProcessor.js added wordProcessor CSS added an image folder with practice img started on wordprocessorapiroutes and html.handlebars

--- routes/wordprocessorApiRoutes.js
+++ routes/wordprocessorApiRoutes.js
@@ -12,3 +12,49 @@
 
 // GET project_content of project
 // PUT change project_content of project
+
+var db = require("../models");
+
+module.exports = function (app) {
+
+
+ // Get all Topics of a Project (of a User)
+
+ app.get("/api/:user/:project/topics", function (req, res) {
+    db.Topic.findAll({
+      where: {
+        ProjectId: req.params.project
+      }
+    }).then(function (dbTopics) {
+      // console.log("topicName: " + dbTopics[0].dataValues.topicName);
+      // console.log("topic id: " + dbTopics[0].dataValues.id);
+      res.json(dbTopics);
+    });
+  });
+
+  // ========================================================
+
+  // Get all Resources of a Topic (of a Project of a User)
+
+  app.get("/api/:user/:project/:topic/resources", function (req, res) {
+    db.Resource.findAll({
+      where: {
+        TopicId: req.params.topic
+      }
+    }).then(function (dbResources) {
+      // console.log("resourceName: " + dbResources[0].dataValues.resourceName);
+      // console.log("resource id: " + dbResources[0].dataValues.id);
+      res.json(dbResources);
+    });
+  });
+
+  // ========================================================
+  // ========================================================
+
+//   PUT to update new user project
+
+
+
+
+//   GET to find stored user project
+}
